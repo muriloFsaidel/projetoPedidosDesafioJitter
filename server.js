0a3c5f1105459b5ec0980c4b1b59c8402ac9652f
Murilo F. Saidel <51511259+muriloFsaidel@users.noreply.github.com>
2026-03-09 23:04:52 -0300

    Adicionando a ferramenta Prisma
    Adicionando a ferramenta Prisma por meio constantes para fazer chamadas ao banco de dados MongoDB

--- server.js
+++ server.js
@@ -7,6 +7,48 @@ const app = library();
 //fazendo com que o express utilize o json como formato de recebimento informações do front-end
 app.use(library.json());
 
+//instanciando a biblioteca do prisma
+const { PrismaClient } = require('@prisma/client');
+
+//armazenado o objeto na constante
+const prisma = new PrismaClient();
+
+// mapeando a rota /order do tipo post para criar um novo pedido no servidor local na porta 3000
+app.post('/order', async (request, response) => {
+
+    // estou desmembrando os dados enviados pela requisição por parte do cliente e armazenando os respectivos valores nas variáveis equivalentes aos campos/chaves do JSON
+    let { numeroPedido, valorTotal, dataCriacao, items } = request.body;
+
+    //fazendo a verificação se os dados são válidos
+    if ((numeroPedido == "" || numeroPedido === undefined) || (valorTotal === undefined) || (dataCriacao == "" || dataCriacao === undefined) || (items.length == 0)) {
+        return response.status(400).send({ mensagem: "algum dos valores não foi preenchido, favor verificar" });
+    }
+    else {
+        //executando operação de criação no banco de dados mongo db na tabela order
+        const pedido = await prisma.order.create({
+            data: {
+                //mapeando os dados recebidos da request para um objeto 
+                orderId: numeroPedido,
+                value: valorTotal,
+                creationDate: new Date(dataCriacao),
+                items: {
+                    set: items.map(produto => ({
+                        productId: produto.idItem,
+                        quantity: produto.quantidadeItem,
+                        price: produto.valorItem
+                    }))
+                },
+                version: 0
+            }
+        });
+        //retorno com status 201 'criado com sucesso' e os dados do pedido
+        return response.status(201).send(pedido);
+    }
+
+});
+
+
+
 
 //Configurando o servidor node.js express para 'ouvir' requisições na porta de serviços 3000, informando que está funcionando
 app.listen(3000, () => {
